3b0726c3c80cfc3349ea455440247b5f83d9aa67
daisuke5325 <daisuke_take@get-it.ne.jp>
2024-12-17 21:00:37 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -8,6 +8,6 @@ let height = 1.7; // 1.7m
 let bmi = weight / (height * height);
 
 // BMI を結果として表示
-console.log("体重: " + weight + "kg");
-console.log("身長: " + height + "m");
+// console.log("体重: " + weight + "kg");
+// console.log("身長: " + height + "m");
 console.log("BMIの値: " + bmi.toFixed(2)); // 小数点2桁に丸める
